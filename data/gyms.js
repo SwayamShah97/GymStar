433b89f340e6970b0ae36a28cd1e543983b7e694
Swayam Shah <swayamshah97@gmail.com>
2021-12-12 14:26:10 -0500

    12/12/21 totally

--- data/gyms.js
+++ data/gyms.js
@@ -199,7 +199,8 @@ async create(userName,gymName, location, phoneNumber, priceRange) {
         arr.push(ret[i].rating)
       }
       let sum = arr.reduce((a,b)=>a+b)
-      overallRating = sum/arr.length; 
+      overallRating = sum/arr.length;
+      Math.round(overallRating * 10)/10; 
       return overallRating
     }
   },
